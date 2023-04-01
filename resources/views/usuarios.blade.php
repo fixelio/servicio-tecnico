bd892cb7cf8e1b5b6f56e7a67f2ee6455c33bf6a
fixelio <julio.molina.m02@gmail.com>
2023-04-01 16:30:34 -0400

    update: assets 'app.css' y 'app.js' agregados

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -5,7 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lista de Usuarios</title>
 
-  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <script src="{{ asset('js/app.js') }}"></script>
+
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
   <ul>
