42181369784f67511248e975fe520574d9a1c64a
fixelio <julio.molina.m02@gmail.com>
2023-04-01 17:31:52 -0400

    update: bootstrap css y js agregado a la carpeta 'public'

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lista de Usuarios</title>
 
-  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
   <ul>
