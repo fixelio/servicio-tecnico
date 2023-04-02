ed43203a0a17c19fef06f1ef5a567785c2dbd6c0
fixelio <julio.molina.m02@gmail.com>
2023-04-02 16:05:34 -0400

    udpate: clases de bootstrap agregadas a formulario

--- resources/views/solicitud-mantenimiento.blade.php
+++ resources/views/solicitud-mantenimiento.blade.php
@@ -1,10 +1,52 @@
-<main>
-  <form action="{{ route('solicitud.post') }}" method="POST">
+<main class="d-flex justify-content-center align-items-center w-75 p-3">
+  <form
+    action="{{ route('solicitud.post') }}"
+    method="POST"
+  >
     @csrf
-    <input type="text" id="num_serie" name="num_serie" placeholder="Número de serie" required>
-    <input type="text" id="marca" name="marca" placeholder="Marca" required>
-    <input type="text" id="modelo" name="modelo" placeholder="Modelo" required>
-    <input type="date" id="fecha_compra" name="fecha_compra" required>
-    <button type="submit">Registrar</button>
+    <div class="mb-3">
+      <label for="num_serie" class="form-label">Número de Serie</label>
+      <input
+        type="text"
+        id="num_serie"
+        name="num_serie"
+        class="form-control"
+        placeholder="Ingresa"
+        required
+      >
+    </div>
+    <div class="mb-3">
+      <label for="marca" class="form-label">Marca</label>
+      <input
+        type="text"
+        id="marca"
+        name="marca"
+        class="form-control"
+        placeholder="Ingresa"
+        required
+      >
+    </div>
+    <div class="mb-3">
+      <label for="modelo" class="form-label">Modelo</label>
+      <input
+        type="text"
+        id="modelo"
+        name="modelo"
+        class="form-control"
+        placeholder="Ingresa"
+        required
+      >
+    </div>
+    <div class="mb-3">
+      <label for="fecha_compra" class="form-label">Fecha de Compra</label>
+      <input
+        type="date"
+        id="fecha_compra"
+        name="fecha_compra"
+        class="form-control"
+        required
+      >
+    </div>
+    <button type="submit" class="btn btn-primary">Registrar</button>
   </form>
 </main>
