dd802ec9506fb9ce2f14f4114934c25e30297b29
fixelio <julio.molina.m02@gmail.com>
2023-04-02 16:48:32 -0400

    update: SolicitudMantenimientoController
    desc: agregada conexión con base de datos

--- app/Http/Controllers/SolicitudMantenimientoController.php
+++ app/Http/Controllers/SolicitudMantenimientoController.php
@@ -12,6 +12,25 @@ class SolicitudMantenimientoController extends Controller
   }
 
   public function crear(Request $request) {
-    
+    $request->validate([
+      'num_serie' => 'required',
+      'marca' => 'required',
+      'modelo' => 'required',
+      'fecha_compra' => 'required',
+    ]);
+
+    $data = $request->all();
+    $check = $this->create($data);
+
+    return view('solicitud-mantenimiento');
+  }
+
+  public function create(array $data) {
+    return SolicitudMantenimiento::create([
+      'num_serie' => $data['num_serie'],
+      'marca' => $data['marca'],
+      'modelo' => $data['modelo'],
+      'fecha_compra' => $data['fecha_compra'],
+    ]);
   }
 }
--- resources/views/solicitud-mantenimiento.blade.php
+++ resources/views/solicitud-mantenimiento.blade.php
@@ -1,55 +1,59 @@
 @extends('layout')
 
 @section('content')
-<div class="d-flex justify-content-center align-items-center w-75 p-3">
-  <form
-    action="{{ route('solicitud.post') }}"
-    method="POST"
-  >
-    @csrf
-    <div class="mb-3">
-      <label for="num_serie" class="form-label">Número de Serie</label>
-      <input
-        type="text"
-        id="num_serie"
-        name="num_serie"
-        class="form-control"
-        placeholder="Ingresa"
-        required
-      >
-    </div>
-    <div class="mb-3">
-      <label for="marca" class="form-label">Marca</label>
-      <input
-        type="text"
-        id="marca"
-        name="marca"
-        class="form-control"
-        placeholder="Ingresa"
-        required
-      >
-    </div>
-    <div class="mb-3">
-      <label for="modelo" class="form-label">Modelo</label>
-      <input
-        type="text"
-        id="modelo"
-        name="modelo"
-        class="form-control"
-        placeholder="Ingresa"
-        required
-      >
-    </div>
-    <div class="mb-3">
-      <label for="fecha_compra" class="form-label">Fecha de Compra</label>
-      <input
-        type="date"
-        id="fecha_compra"
-        name="fecha_compra"
-        class="form-control"
-        required
-      >
-    </div>
-    <button type="submit" class="btn btn-primary">Registrar</button>
-  </form>
-</div>
+<section>
+  <div class="container-sm d-flex justify-content-center align-items-center flex-column">
+    <form
+      action="{{ route('solicitud.post') }}"
+      method="POST"
+      class="row mt-5 g-3"
+    >
+      @csrf
+      <div class="col-12 col-lg-6 col-xl-4 mb-3">
+        <label for="num_serie" class="form-label">Número de Serie</label>
+        <input
+          type="text"
+          id="num_serie"
+          name="num_serie"
+          class="form-control"
+          placeholder="Ingresa"
+          required
+        >
+      </div>
+      <div class="col-12 col-lg-6 col-xl-4 mb-3">
+        <label for="marca" class="form-label">Marca</label>
+        <input
+          type="text"
+          id="marca"
+          name="marca"
+          class="form-control"
+          placeholder="Ingresa"
+          required
+        >
+      </div>
+      <div class="col-12 col-lg-6 col-xl-4 mb-3">
+        <label for="modelo" class="form-label">Modelo</label>
+        <input
+          type="text"
+          id="modelo"
+          name="modelo"
+          class="form-control"
+          placeholder="Ingresa"
+          required
+        >
+      </div>
+      <div class="col-12 col-lg-6 col-xl-4 mb-3">
+        <label for="fecha_compra" class="form-label">Fecha de Compra</label>
+        <input
+          type="date"
+          id="fecha_compra"
+          name="fecha_compra"
+          class="form-control"
+          required
+        >
+      </div>
+      <button type="submit" class="btn btn-primary">Registrar</button>
+    </form>
+  </div>
+</section>
+@endsection
