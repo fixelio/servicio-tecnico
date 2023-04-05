875514ceb74d32a8cf9b95998a111eb52bc1fafc
fixelio <julio.molina.m02@gmail.com>
2023-04-05 17:04:40 -0400

    vista de listado de ordenes

--- resources/views/solicitudes/listado.blade.php
+++ resources/views/solicitudes/listado.blade.php
@@ -4,7 +4,23 @@
   <section class="my-5">
     <div class="container d-flex justify-content-center align-items-center flex-column">
       <div class="w-75 mt-5">
-        <h3>Aún estamos trabajando en esta página</h3>
+        <h3>Listado de órdenes</h3>
+      </div>
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Código</th>
+              <th scope="col">Cliente</th>
+              <th scope="col">Artículo</th>
+              <th scope="col">Estado</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
       </div>
     </div>
   </section>
